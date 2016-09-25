f8c7a67d0385c8a7aa7f260a0f1e1198c90c1cb3
Satvik Vishnubhatta <satvik.2692@gmail.com>
2016-09-25 16:00:01 -0700

    Update TrieNode.java

--- TrieNode.java
+++ TrieNode.java
@@ -1,18 +1,15 @@
 import java.util.*;
 class TrieNode{
- char data;
- boolean is_leaf;
- //TrieNode child[];
- HashMap<Integer,TrieNode> children;
+ char data;// TO store a character of the string.
+ boolean is_leaf;// To mark a word has been completed.
+ //TrieNode child[]; We can use array of size 26 for mapping children.
+ HashMap<Integer,TrieNode> children;//Hashmap is used for mapping children with key as Alphabets and value as the character of the string.
  
  TrieNode(){
-	 //child=new TrieNode[26];
+	 
 	 children=new HashMap<Integer,TrieNode>(26);
 	 data='0';
 	 is_leaf=false;
  }
- /* TrieNode(char c){
-	 data=c;
-	 
- } */
+
 }
